c82769ef529b919b5fa7b452dea20c5c4494af3e
WangJing <wangjingnic1993@gmail.com>
2019-02-22 11:23:50 +0800

    后台用户页面；

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,13 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name')->nullable();
             $table->string('phone')->unique()->comment('手机号码');
+            $table->tinyInteger('device_type')->nullable()->comment('设备类型 0:h5 1:ios 2:android');
+            $table->tinyInteger('status')->default(1)->comment('用户状态 0：锁定 1：未激活 2：已激活');
+            $table->tinyInteger('channel_id')->default(0)->comment('渠道ID 默认0:自有平台');
             $table->rememberToken();
             $table->timestamps();
         });
+        \DB::statement("ALTER TABLE `users` comment '用户信息登记表'");
     }
 
     /**
--- database/migrations/2019_02_21_035615_create_merchants_table.php
+++ database/migrations/2019_02_21_035615_create_merchants_table.php
@@ -19,14 +19,14 @@ class CreateMerchantsTable extends Migration
             $table->string('key_name')->unique()->comment('商户标识');
             $table->string('name')->comment('商户名称');
             $table->unsignedInteger('max_limit')->comment('最高额度');
-            $table->string('description')->nullable()->comment('车品描述');
+            $table->string('description')->nullable()->comment('商户描述');
             $table->float('rate')->nullable()->comment('利息低至');
-            $table->string('url')->nullable()->comment('推广 URL');
+            $table->string('url')->nullable()->comment('推广URL');
             $table->string('label_first')->nullable()->comment('标签1');
             $table->string('label_second')->nullable()->comment('标签2');
             $table->string('label_third')->nullable()->comment('标签3');
             $table->unsignedInteger('count_click')->default(0)->comment('点击数');
-            $table->unsignedInteger('week_loan')->default(0)->comment('本周下款人数');
+            $table->unsignedInteger('count_week')->default(0)->comment('本周下款人数');
             $table->timestamps();
         });
         \DB::statement("ALTER TABLE `merchants` comment '商户信息登记表'");
